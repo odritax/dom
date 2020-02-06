afad2b8b12cc746587951da86db3bc336dc197d7
odra <odraromina>
2020-02-06 11:32:39 -0400

    NEW: primer commit subiendo archivos

---
+++  traduccion-formulario_/js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('form-signin-heading').innerHTML="Por favor inicia sesión"
+document.getElementById('inputEmail').placeholder="Correo electronico";
+document.getElementById('inputPassword').placeholder="Contraseña";
+document.getElementsByTagName('span')[0].innerHTML="Recordar datos";
+document.getElementsByClassName('btn btn-lg btn-primary btn-block submit-btn')[0].innerHTML="Iniciar Sesión";
